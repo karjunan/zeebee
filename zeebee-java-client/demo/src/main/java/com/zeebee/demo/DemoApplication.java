658d232127635bad8f988d9dd195e2c3a27e1070
krishnakumarArjunan <arjunankrishnakumar1@gmail.com>
2021-02-28 20:21:00 +0530

    bpmn file commited

--- zeebee-java-client/demo/src/main/java/com/zeebee/demo/DemoApplication.java
+++ zeebee-java-client/demo/src/main/java/com/zeebee/demo/DemoApplication.java
@@ -2,6 +2,8 @@ package com.zeebee.demo;
 
 import io.zeebe.client.ZeebeClient;
 import io.zeebe.client.api.response.DeploymentEvent;
+import io.zeebe.client.api.response.WorkflowInstanceEvent;
+import io.zeebe.client.api.worker.JobWorker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -15,23 +17,43 @@ public class DemoApplication {
 	}
 
 	public static void run() {
-		final String gatewayAddress = "192.168.0.111:26500";
+		final String gatewayAddress = "192.168.225.53:26500";
+//		final String gatewayAddress = "localhost:26500";
 
 		final ZeebeClient client =
 				ZeebeClient.newClientBuilder()
+						.usePlaintext()
 						.gatewayAddress(gatewayAddress)
 						.build();
 
 		System.out.println("Connected");
 		final DeploymentEvent deployment = client.newDeployCommand()
-				.addResourceFile(".//order-process.bpmn")
+				.addResourceFile(".\\bpmn\\order-process.bpmn")
 				.send()
 				.join();
 
 		final int version = deployment.getWorkflows().get(0).getVersion();
 		System.out.println("Workflow deployed. Version: " + version);
-		client.close();
-		System.out.println("Closed.");
+
+		final WorkflowInstanceEvent wfInstance = client.newCreateInstanceCommand()
+				.bpmnProcessId("Order_Placed")
+				.latestVersion()
+				.send()
+				.join();
+		System.out.println("Work flow instance ID :: ==> "+ wfInstance.getWorkflowInstanceKey());
+
+//		final JobWorker jobWorker = client.newWorker()
+//				.jobType("payment-service")
+//				.handler(((jobClient, job) -> {
+//					System.out.println("I am collecting the money");
+//
+//					jobClient.newCompleteCommand(job.getKey())
+//							.send()
+//							.join();
+//				})).open();
+//		jobWorker.close();
+//		client.close();
+//		System.out.println("Closed.");
 	}
 
 }
